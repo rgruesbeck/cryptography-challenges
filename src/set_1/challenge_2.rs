3cc89e2d260a4e7947e281cb3acb20ffc28adb35
Ron Gruesbeck <rongruesbeck@protonmail.com>
2022-10-05 09:20:48 -0400

    challenge 2 fixed length xor

---
+++ src/set_1/challenge_2.rs
@@ -0,0 +1,54 @@
+/*
+    https://cryptopals.com/sets/1/challenges/2
+
+    Fixed XOR
+    Write a function that takes two equal-length buffers and produces their XOR combination.
+    If your function works properly, then when you feed it the string: 
+*/
+
+fn fixed_xor(a: &str, b: &str) -> String {
+    let char_set = "0123456789abcdef";
+    let mut result = String::new();
+
+    fn decode_hex_byte(n: u8) -> u8 {
+        let a = u8::from(n & 0b_0_111_0000) >> 4;
+        let b = u8::from(n & 0b_0_000_1111);
+        let c = match a {
+            3 => b,
+            6 => b + 9,
+            _ => 0
+        };
+        c
+    }
+
+    let a_bytes = a.as_bytes();
+    let b_bytes = b.as_bytes();
+
+    let mut i = 0;
+    for a_byte in a_bytes {
+        let b_byte = &b_bytes[i];
+        let x_byte = u8::from(decode_hex_byte(*a_byte) ^ decode_hex_byte(*b_byte));
+        match char_set.chars().nth(x_byte.into()) {
+            Some(x) => {
+                result.push(x);
+            }
+            None => ()
+        }
+        i += 1;
+    }
+
+    result
+}
+
+pub fn test() {
+    let a = "1c0111001f010100061a024b53535009181c";
+    let b = "686974207468652062756c6c277320657965";
+    let expected = "746865206b696420646f6e277420706c6179";
+
+    let result = fixed_xor(a, b);
+    assert_eq!(result, expected);
+
+    println!("\nChallenge 2: Fixed XOR");
+    println!("exp: {}", expected);
+    println!("res: {}", result);
+}
--- src/set_1/mod.rs
+++ src/set_1/mod.rs
@@ -1,5 +1,7 @@
 mod challenge_1;
+mod challenge_2;
 
 pub fn test() {
     challenge_1::test();
+    challenge_2::test();
 }
