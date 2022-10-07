f74ef5109bc3fa4623d77c94fe4da39dfc93af26
Ron Gruesbeck <rongruesbeck@protonmail.com>
2022-10-07 09:52:27 -0400

    update mod.rs

--- src/set_1/mod.rs
+++ src/set_1/mod.rs
@@ -1,9 +1,10 @@
 mod challenge_1;
 mod challenge_2;
 mod challenge_3;
+mod challenge_4;
 
 pub fn test() {
     challenge_1::test();
     challenge_2::test();
-    challenge_3::test();
+    challenge_4::test();
 }
