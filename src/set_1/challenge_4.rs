1deed07f7f9c3ae4e8e63893b5fd80e12883f480
Ron Gruesbeck <rongruesbeck@protonmail.com>
2022-10-07 09:52:15 -0400

    challenge 4: start

---
+++ src/set_1/challenge_4.rs
@@ -0,0 +1,18 @@
+/*
+    https://cryptopals.com/sets/1/challenges/4
+
+    Detect single-character XOR
+
+    One of the 60-character strings in this file has been encrypted by single-character XOR.
+    Find it.
+
+    (Your code from #3 should help.)
+
+*/
+
+pub fn test() {
+
+    println!("\nChallenge 4: Detect single-character XOR");
+    // println!("exp: {}", expected);
+    // println!("res: {}", result);
+}
