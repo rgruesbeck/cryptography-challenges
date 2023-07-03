d6402f4665543ac04303ca42f41373738015a54d
Ron Gruesbeck <rongruesbeck@gmail.com>
2023-07-03 13:57:42 -0400

    hex to base64

--- src/set_1/challenge_1.rs
+++ src/set_1/challenge_1.rs
@@ -8,11 +8,60 @@
 
 
 /*
+   hex
    hex char set: "0123456789abcdef"
+   4 bits per char
+
+   b64
    b64 char set: "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/"
+
 */
 fn hex_to_base64(s: &str) -> String {
-    let mut result = new String();
+    let char_set_64 = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
+    let mut result = String::new();
+
+    fn to_hex(byte: u8) -> u8 {
+        return match byte & 0b1111_0000 {
+            0b0110_0000 => (byte & 0b0000_1111) + 9,
+            _ => byte & 0b0000_1111
+        }
+    }
+
+    fn process_buffer(mut result: &mut String, set: &str, buf: [u8; 3], idx: usize) {
+        match idx {
+            1 => {
+                let i = ((buf[0] & 0b0000_1111) << 2) + ((buf[1] & 0b0000_1100) >> 2);
+                match set.chars().nth(i.into()) {
+                    Some(c) => {
+                        result.push(c);
+                    },
+                    None => ()
+                }
+            }
+            2 => {
+                let i = ((buf[1] & 0b0000_0011) << 4) + (buf[2] & 0b0000_1111);
+                match set.chars().nth(i.into()) {
+                    Some(c) => {
+                        result.push(c);
+                    },
+                    None => ()
+                }
+            }
+            _ => {},
+        }
+    }
+
+    // iterate over bytes converting hex encoded bytes to base64
+    let mut i: usize = 0;
+    let mut buffer: [u8; 3] = [0; 3];
+    for byte in s.bytes() {
+        buffer[i] = to_hex(byte);
+        process_buffer(&mut result, char_set_64, buffer, i);
+
+        i += 1;
+        if i > 2 { i = 0 }
+    }
+
     return result;
 }
 
--- src/set_1/mod.rs
+++ src/set_1/mod.rs
@@ -1,15 +1,5 @@
 mod challenge_1;
-mod challenge_2;
-mod challenge_3;
-mod challenge_4;
-mod challenge_5;
-mod challenge_6;
 
 pub fn test() {
     challenge_1::test();
-    challenge_2::test();
-    challenge_3::test();
-    challenge_4::test();
-    challenge_5::test();
-    challenge_6::test();
 }
