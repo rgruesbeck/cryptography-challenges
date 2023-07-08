7fa9be9b3eafa51a2d2d2a570d32999b58c5a203
Ron Gruesbeck <rongruesbeck@gmail.com>
2023-07-08 10:06:57 -0400

    challenge_3 score

--- src/set_1/challenge_3.rs
+++ src/set_1/challenge_3.rs
@@ -28,15 +28,55 @@ https://mathworld.wolfram.com/XOR.html
 https://bitwisecmd.com/
 */
 
-fn score(s: &str) -> u8 {
-    0
+use std::iter::zip;
+use indexmap::IndexMap;
+
+// https://docs.rs/indexmap/1.6.0/indexmap/map/struct.IndexMap.html#examples
+fn get_frequency(string: &str) -> IndexMap<char, u32> {
+    let mut frequency = IndexMap::new();
+    // build frequency map
+    for ch in string.chars() {
+        *frequency.entry(ch).or_insert(0) += 1;
+    }
+
+    // sort frequency map
+    frequency.sort_by(|ka, va, kb, vb| {
+        vb.cmp(va)
+    });
+
+    return frequency;
 }
 
+fn score(freq: IndexMap<char, u32>) -> u32 {
+    let english = " etaoinsh";
+    let mut score = 0;
+    for letters in zip(english.chars(), freq.clone().into_keys()) {
+        let (a, b) = letters;
+        score += u32::from(a) ^ u32::from(b);
+    }
+    score
+}
+
+fn check(string: &str) -> String {
+    let frequency = get_frequency(&string);
+    let res = String::new();
+
+    for ch in string.bytes() {
+    }
+
+    println!("freq: {:?}", frequency);
+    println!("score: {:?}", score(frequency));
+    return res;
+}
+
+
 pub fn test() {
+    let a: &str = "all work and no play makes jack a dull boy";
     let s: &str = "1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736";
     let score = "a";
 
     println!("\nChallenge 3: Single-byte XOR cipher");
+    println!("try: {}", check(s));
     //println!("exp: {}", expected);
     // println!("res: {}", result);
 }
